1d3da3efdbb992c277e3c325c4f825eccfe68f96
Michael Mior <mike@urbanorca.com>
2012-09-02 18:47:20 -0400

    Use Handlebars instead of EJS

--- config/environments/all.js
+++ config/environments/all.js
@@ -12,18 +12,10 @@ module.exports = function() {
   // Configure application settings.  Consult the Express API Reference for a
   // list of the available [settings](http://expressjs.com/api.html#app-settings).
   this.set('views', __dirname + '/../../app/views');
-  this.set('view engine', 'ejs');
+  this.set('view engine', 'hbs');
 
-  // Register EJS as a template engine.
-  this.engine('ejs', require('ejs').__express);
-
-  // Override default template extension.  By default, Locomotive finds
-  // templates using the `name.format.engine` convention, for example
-  // `index.html.ejs`  For some template engines, such as Jade, that find
-  // layouts using a `layout.engine` notation, this results in mixed conventions
-  // that can cuase confusion.  If this occurs, you can map an explicit
-  // extension to a format.
-  /* this.format('html', { extension: '.jade' }) */
+  // Register Handlebars as a template engine for html.
+  this.engine('hbs', require('consolidate').handlebars);
 
   // Register formats for content negotiation.  Using content negotiation,
   // different formats can be served as needed by different clients.  For
