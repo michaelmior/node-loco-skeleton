6ba02b8f3e42f0677b2a5ccbb9cc46d882a572d1
Michael Mior <mike@joinbunch.com>
2012-09-12 21:49:45 -0400

    Add extra comments to development environment

--- config/environments/development.js
+++ config/environments/development.js
@@ -4,11 +4,13 @@ var express = require('express')
 module.exports = function() {
   this.set('mongodb uri', 'mongodb://localhost/test');
 
+  // Set up nice defaults for console logging
   var logger = this.get('logger');
   logger.add(
     winston.transports.Console
   ).cli();
 
+  // Log all queries going through MongoDB
   require('mongoose').set('debug', function(collection, method, query, options) {
     logger.debug('mongoose - ' + collection + '.' + method + '(' + JSON.stringify(query) + ') - ' + JSON.stringify(options));
   });
