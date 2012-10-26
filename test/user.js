101deb684c822c934ef95ce126d66b93a2aa3669
Michael Mior <mike@joinbunch.com>
2012-10-26 19:14:55 -0400

    Add tests for the User model

---
+++ test/user.js
@@ -0,0 +1,33 @@
+var vows = require('vows'),
+    should = require('should'),
+    bcrypt = require('bcrypt'),
+    User = require('../app/models/user.js');
+
+
+vows.describe('User').addBatch({
+  'when creating a user': {
+    topic: function() {
+      var cb = this.callback;
+      User.prototype.save = function() { cb(null, this); };
+      User.registerUser('test@example.com', 'password', function() {});
+    },
+    'the correct email is saved': function(err, user) {
+      user.email.should.equal('test@example.com');
+    },
+    'a valid password hash is created': function(err, user) {
+      bcrypt.compareSync('password', user.hash).should.be.true;
+    },
+    'checking a valid password': {
+      topic: function(user) { user.validPassword('password', this.callback); },
+      'authenticates the user': function(err, valid) {
+        valid.should.be.true;
+      }
+    },
+    'checking an invalid password': {
+      topic: function(user) { user.validPassword('wrongpassword', this.callback); },
+      'does not authenticate the user': function(err, valid) {
+        valid.should.be.false;
+      }
+    }
+  }
+}).export(module);
